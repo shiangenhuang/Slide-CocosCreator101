21b9b03532f5ac976b13fb02b7782e0a4a90b8d0
shiangenhuang <shiangenhuang@igs.com.tw>
2026-03-20 19:03:45 +0800

    init

--- assets/md-Claze7IO.js
+++
@@ -1,9 +0,0 @@
-import{_ as r}from"./slidev/CodeBlockWrapper.vue_vue_type_script_setup_true_lang-CXhmPU8u.js";import{o as p,b as c,w as n,d as u,m as d,g as s,ad as a,v as m,x as f,T as t}from"./modules/vue-Bb5JUXOJ.js";import{I as _}from"./slidev/default-o8JbaaNk.js";import{u as k,f as g}from"./slidev/context-u9WVZ7K4.js";import"./modules/unplugin-icons-OiqrM49x.js";import"./index-DoqlTbM8.js";import"./modules/shiki-CgH6uPBv.js";import"./slidev/TitleIcon.vue_vue_type_script_setup_true_lang-D4hgpOf-.js";const V={__name:"slides.md__slidev_21",setup(h){const{$clicksContext:l,$frontmatter:i}=k();return l.setup(),(v,e)=>{const o=r;return p(),c(_,m(f(t(g)(t(i),20))),{default:n(()=>[u(o,d({},{title:"",ranges:[]}),{default:n(()=>[...e[0]||(e[0]=[s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-text"},[s("span",{class:"line"},[s("span",null,"[Node 命中條件]")]),a(`
-`),s("span",{class:"line"},[s("span",null,"│")]),a(`
-`),s("span",{class:"line"},[s("span",null,"├─ active = true ✅ → 節點可參與檢測")]),a(`
-`),s("span",{class:"line"},[s("span",null,"├─ UITransform.contains(point) = true ✅ → 點在範圍內")]),a(`
-`),s("span",{class:"line"},[s("span",null,"├─ 通過 Mask / Clipping 區域 ✅ → 沒被裁掉")]),a(`
-`),s("span",{class:"line"},[s("span",null,"├─ BlockInputEvents（若存在）✅ → 可攔截事件 (透明UI也會觸發或攔截事件)")]),a(`
-`),s("span",{class:"line"},[s("span",null,"│")]),a(`
-`),s("span",{class:"line"},[s("span",null,"▼")]),a(`
-`),s("span",{class:"line"},[s("span",null,"👉 命中 → 成為 target（並停止向下搜尋）")])])],-1)])]),_:1},16)]),_:1},16)}}};export{V as default};
---
+++ assets/md-DUHDU6hu.js
@@ -0,0 +1,14 @@
+import{_ as d}from"./slidev/CodeBlockWrapper.vue_vue_type_script_setup_true_lang-v1a4fIFZ.js";import{o as c,b as u,w as n,g as s,d as t,m as i,ad as e,v as f,x as m,T as p}from"./modules/vue-Bb5JUXOJ.js";import{I as k}from"./slidev/default-tTHMBCeU.js";import{u as g,f as h}from"./slidev/context-D0hDbZWm.js";import"./modules/unplugin-icons-OiqrM49x.js";import"./index-BIRZb7jU.js";import"./modules/shiki-CgH6uPBv.js";import"./slidev/TitleIcon.vue_vue_type_script_setup_true_lang-D4hgpOf-.js";const N={__name:"slides.md__slidev_6",setup(_){const{$clicksContext:o,$frontmatter:r}=g();return o.setup(),(b,a)=>{const l=d;return c(),u(k,f(m(p(h)(p(r),5))),{default:n(()=>[a[2]||(a[2]=s("h2",null,"節點生命週期",-1)),t(l,i({},{title:"",ranges:[]}),{default:n(()=>[...a[0]||(a[0]=[s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-text"},[s("span",{class:"line"},[s("span",null,"onLoad (once)")]),e(`
+`),s("span",{class:"line"},[s("span",null," → onEnable (Can be multiple)")]),e(`
+`),s("span",{class:"line"},[s("span",null,"   → start (once)")]),e(`
+`),s("span",{class:"line"},[s("span",null,"     → update (every frame)")]),e(`
+`),s("span",{class:"line"},[s("span",null,"     → lateUpdate (every frame)")]),e(`
+`),s("span",{class:"line"},[s("span",null," → onDisable (Can be multiple)")]),e(`
+`),s("span",{class:"line"},[s("span",null," → onDestroy (once)")])])],-1)])]),_:1},16),t(l,i({},{title:"",ranges:[]}),{default:n(()=>[...a[1]||(a[1]=[s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-text"},[s("span",{class:"line"},[s("span",null,"| Lifecycle   | 說明                        |")]),e(`
+`),s("span",{class:"line"},[s("span",null,"|------------|--------------------------   |")]),e(`
+`),s("span",{class:"line"},[s("span",null,"| onLoad     | 適合做 `getComponent`、`find` |")]),e(`
+`),s("span",{class:"line"},[s("span",null,"| start      | 適合做跨節點參照               |")]),e(`
+`),s("span",{class:"line"},[s("span",null,"| update     | ⚠️效能敏感區域                |")]),e(`
+`),s("span",{class:"line"},[s("span",null,"| lateUpdate | ⚠️效能敏感區域                |")]),e(`
+`),s("span",{class:"line"},[s("span",null,"| onDisable  | 適合做清理資源                 |")]),e(`
+`),s("span",{class:"line"},[s("span",null,"| onDestroy  | 適合做清理資源                 |")])])],-1)])]),_:1},16)]),_:1},16)}}};export{N as default};
